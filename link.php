b6e2e3eb97fd4dd48d8eb8f9d8cdec96050bc38d
daortellado <daortellado@gmail.com>
2017-04-19 13:13:57 -0700

    initial html php commit

---
+++ link.php
@@ -0,0 +1,13 @@
+﻿<?php
+$links = array();
+$links[] = 'http://www.espn.com';
+$links[] = 'http://www.google.com';
+ 
+shuffle($links);
+ 
+$i        = rand(0, (count($links) - 1));
+$link    = $links[$i];
+ 
+header("Location: {$link}");
+exit;
+?>;
