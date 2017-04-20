de17aed39b03e01b8c4257da8fdc7eca6b070442
Vincent Dorie <vdorie@gmail.com>
2017-04-20 18:14:56 -0400

    uses a cookie to track user sessisons and ids

--- link.php
+++ link.php
@@ -1,13 +1,27 @@
-﻿<?php
-$links = array();
-$links[] = 'http://www.surveygizmo.com/s3/3511947/AB-953-forms-test-combo-repeat?sguid=';
-$links[] = 'http://www.surveygizmo.com/s3/3506584/AB-953-forms-copy-2?sguid=';
- 
-shuffle($links);
- 
-$i        = rand(0, (count($links) - 1));
-$link    = $links[$i];
- 
-header("Location: {$link}".rand(1000,9999));
+<?php
+
+## create a new link
+if ($_COOKIE["cjsc"] == "") {
+  $links = array();
+  $links[] = 'http://www.surveygizmo.com/s3/3511947/AB-953-forms-test-combo-repeat?sguid=';
+  $links[] = 'http://www.surveygizmo.com/s3/3506584/AB-953-forms-copy-2?sguid=';
+  
+  shuffle($links);
+  
+  $i        = rand(0, (count($links) - 1));
+  $link    = $links[$i];
+  
+  $link = $link.rand(1000,9999);
+  
+  setcookie("cjsc", $link);
+} else {
+  $link = $_COOKIE["cjsc"];
+  
+  ## unsets the cookie
+  #setcookie("cjsc", "", time() - 1);
+}
+
+header("Location: {$link}");
+
 exit;
-?>;
+?>
