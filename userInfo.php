46ec352c4fb0d78b754611371ca22806d7dbd718
daortellado <daortellado@gmail.com>
2017-04-21 11:31:02 -0700

    delete unneeded userdb php file

--- userInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
- 		include_once('db.php');
-		$name = $_POST['name'];
-		$age = $_POST['time'];
-		if(mysql_query("INSERT INTO user VALUES('$name', '$time')"))
-         echo "Successfully Inserted";
-		else
-		echo "Insertion Failed";
-?>
